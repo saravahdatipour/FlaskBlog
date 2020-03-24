f085e50102a7cd0e00d8436e85bf202c5bacd51e
lerunaway <54913233+lerunaway@users.noreply.github.com>
2020-03-25 02:05:21 +0430

    'sessions'

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -3,3 +3,9 @@ from . import admin
 @admin.route('/')
 def admin_index():
     return 'welcome to admin index'
+
+@admin.route('/login/')
+def login():
+    session['name']='sara'
+    print(session)
+    return '1'
